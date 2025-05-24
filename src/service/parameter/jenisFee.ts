a4f1683ff801e2b4e71a0f713a9462f4b3c4f015
farhan rafid syauqi <farhanrafid97@gmail.com>
2025-05-24 18:09:00 +0700

    chore: remove unsued file

--- src/service/parameter/jenisFee.ts
+++
@@ -1,8 +0,0 @@
-import { axiosApiMCRGLCasa } from '@/lib/axios';
-import { ResponseGetParamJenisFee } from '@/schema/MassDebet/pengajuan';
-
-export const getParamJenisFee = async () => {
-  const { data } = await axiosApiMCRGLCasa.get<ResponseGetParamJenisFee>('/paramFeeTypes');
-
-  return data.data;
-};
