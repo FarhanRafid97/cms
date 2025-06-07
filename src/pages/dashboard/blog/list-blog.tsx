ff4f6f14ac11e370316b19f63c248d4d822ebb6c
farhan rafid syauqi <farhanrafid97@gmail.com>
2025-06-08 01:37:30 +0700

    feat: create ListBlog component for displaying blog list

---
+++ src/pages/dashboard/blog/list-blog.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ListBlog() {
+  return (
+    <div>
+      <h1>Blog List</h1>
+      {/* Add your blog list content here */}
+    </div>
+  );
+}
