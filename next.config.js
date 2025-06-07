b6da8308fe61aa91dd2290bf843a7c9cf64b7405
farhan rafid syauqi <farhanrafid97@gmail.com>
2025-06-08 00:51:41 +0700

    chore: remove unused images and clean up configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'bristars.bri.co.id',
-        port: '',
-        pathname: '/bristars/foto/get/**',
-      },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
